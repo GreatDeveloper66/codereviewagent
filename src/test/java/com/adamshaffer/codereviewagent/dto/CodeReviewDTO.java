958f5bc34621d3a9f51b167547ebce33d21ef8ec
GreatDeveloper66 <ashaffe@gmail.com>
2026-05-28 21:33:12 -0500

    Add constructor for CodeReviewDTO

--- src/test/java/com/adamshaffer/codereviewagent/dto/CodeReviewDTO.java
+++ src/test/java/com/adamshaffer/codereviewagent/dto/CodeReviewDTO.java
@@ -6,6 +6,9 @@ import lombok.Data;
 
 @Data
 public class CodeReviewDTO {
+    public CodeReviewDTO(long l, String fileName2, String language2, String codeReview, LocalDateTime now) {
+        //TODO Auto-generated constructor stub
+    }
     private Long id;
     private String fileName;
     private String language;
