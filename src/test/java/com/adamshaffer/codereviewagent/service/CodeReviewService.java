f8718e2d9639a5c435e9bfe5813154c113209a7f
GreatDeveloper66 <ashaffe@gmail.com>
2026-05-28 21:32:54 -0500

    Add CodeReviewService

---
+++ src/test/java/com/adamshaffer/codereviewagent/service/CodeReviewService.java
@@ -0,0 +1,25 @@
+package com.adamshaffer.codereviewagent.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.adamshaffer.codereviewagent.dto.CodeReviewDTO;
+import com.adamshaffer.codereviewagent.model.CodeReviewModel;
+import com.adamshaffer.codereviewagent.repository.CodeReviewRepository;
+
+@Service
+public class CodeReviewService {
+    private final CodeReviewRepository codeReviewRepository;
+    private final AIClientService aiClientService;
+    public CodeReviewService(CodeReviewRepository codeReviewRepository, AIClientService aiClientService) {
+        this.codeReviewRepository = codeReviewRepository;
+        this.aiClientService = aiClientService;
+    }
+
+    public CodeReviewDTO getCodeReview(String fileName, String language) {
+        String codeReview = aiClientService.getCodeReview(fileName, language);
+        codeReviewRepository.save(new CodeReviewModel(fileName, language, codeReview, LocalDateTime.now()));
+        return new CodeReviewDTO(1L, fileName, language, codeReview, LocalDateTime.now());
+    }
+}
