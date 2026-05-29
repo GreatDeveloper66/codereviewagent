a80d383391e034eeb7b3f41fe289c43afd2e2412
GreatDeveloper66 <ashaffe@gmail.com>
2026-05-29 13:11:23 -0500

    Create AnalyzeRequest

---
+++ src/main/java/com/adamshaffer/codereviewagent/controller/AnalyzeRequest.java
@@ -0,0 +1,21 @@
+package com.adamshaffer.codereviewagent.controller;
+
+
+public class AnalyzeRequest {
+
+    public Object getContent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getContent'");
+    }
+
+    public Object getLanguage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLanguage'");
+    }
+
+    public String getFileName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFileName'");
+    }
+
+}
