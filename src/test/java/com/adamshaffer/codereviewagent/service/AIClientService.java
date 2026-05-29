7bb673b4856455da4fdf501405b8d3fa944496f0
GreatDeveloper66 <ashaffe@gmail.com>
2026-05-28 21:33:49 -0500

    add getCodeReview method for AIClientService

---
+++ src/test/java/com/adamshaffer/codereviewagent/service/AIClientService.java
@@ -0,0 +1,10 @@
+package com.adamshaffer.codereviewagent.service;
+
+public class AIClientService {
+
+    public String getCodeReview(String fileName, String language) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCodeReview'");
+    }
+
+}
