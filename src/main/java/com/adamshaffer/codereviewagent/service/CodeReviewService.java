c157c20a095c648597dcb632a1393443dae28686
GreatDeveloper66 <ashaffe@gmail.com>
2026-05-29 16:38:22 -0500

    add getHistory()

--- src/main/java/com/adamshaffer/codereviewagent/service/CodeReviewService.java
+++ src/main/java/com/adamshaffer/codereviewagent/service/CodeReviewService.java
@@ -1,6 +1,7 @@
 package com.adamshaffer.codereviewagent.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -21,5 +22,10 @@ public class CodeReviewService {
         String codeReview = aiClientService.getCodeReview(fileName, language);
         codeReviewRepository.save(new CodeReviewModel(fileName, language, codeReview, LocalDateTime.now()));
         return new CodeReviewDTO(1L, fileName, language, codeReview, LocalDateTime.now());
+    }
+
+    public List<CodeReviewDTO> getHistory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHistory'");
     }
 }
